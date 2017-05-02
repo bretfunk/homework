5ae2d07168919644b0f146d1b92278c13ad38862
BFunk <bret.e.funk@gmail.com>
2017-05-02 09:20:05 -0600

    change comments

--- pseudocode.rb
+++ pseudocode.rb
@@ -1,15 +1,17 @@
+=begin
 If you don’t input anything (just hit enter) they respond with HELLO?!
 If you ask a question with any lower-case letters, they respond with I AM HAVING A HARD TIME HEARING YOU.
 If you ask a question in all upper-case letters, they respond with NO, THIS IS NOT A PET STORE
 The first time you say GOODBYE! they say ANYTHING ELSE I CAN HELP WITH?
 The second time you say GOODBYE! they say THANK YOU FOR CALLING! and the program exits.
 To run the program, you would enter in your command line:
+=end
 
 phone_answer = gets.chomp.to_s
-flag = false
+counter = 0
 
-while flag == false
-if phone_answer.empty?
+while counter < 2
+  if phone_answer.empty?
   return "HELLO!"
   elsif phone_answer != phone_answer.upcase
   return "I AM HAVING A HARD TIME HEARING YOU."
@@ -17,9 +19,12 @@ if phone_answer.empty?
   return "NO, THIS IS NOT A PET STORE"
   elsif phone_answer == "GOODBYE!"
   return "ANYTHING ELSE I CAN HELP WITH?"
-  flag = true
+  counter += 1
   end
 
-phone_answer2 = gets.chomp.to_s
- if phone_answer2 == "GOODBYE"
- THANK YOU FOR CALLING!
+   if phone_answer == "GOODBYE"
+   THANK YOU FOR CALLING!
+   counter += 1
+   end
+
+ end
