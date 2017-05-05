39ac04307bae9cd3b046dba7e442fa233f48e15f
BFunk <bret.e.funk@gmail.com>
2017-05-05 09:42:21 -0600

    update

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,38 +1,13 @@
-#Bubble sort
-# comparison of two values that moves higher value to a higher index in an array
+array = [9, 1, 5, 7, 2, 6]
+n = array.length
 
-class BubbleSort
-  def sort(input)
-
-    #check first value
-    previous_value = input[0]
-    #check second value
-    current_value = input[1]
-
-    #compare values looking for higher
-    if previous_value > current_value
-      require 'pry'; binding.pry
-      [current_value, previous_value]
-    else
-      #do noting
-
-
-
-    # if ifrst value is greater than second value swap places
-    # else do nothing
+n.times do
+  n.times do |x|
+    if array[x + 1] != nil && array[x] > array[x + 1]
+      then
+      array[x], array[x + 1] = array[x + 1], array[x]
     end
-
-
-
-
-    puts input.inspect
-
   end
 end
 
-
-
-sorter = BubbleSort.new
-#=> #<BubbleSort:0x007f81a19e94e8>
-sorter.sort(["d", "b", "a", "c"])
-#=> ["a", "b", "c", "d"]
+puts array
--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,42 +1,28 @@
-#cut off first digit
-#compare digit to everything inside new array1
-#place new digit
-#return new array
 
+array = [1, 3, 5, 6, 2, 2, 2, 2, 9, 0, 0, 0, 0, 4, 5, 6, 6, 6, 1, 1, 1, 1]
+new_array = [4, 4, 4, 4]
 
+    array.length.times do
+      counter = 0
+      removed_num = array.shift
 
+      while counter <= new_array.length
+        if new_array[counter + 1] != nil
+          if removed_num <= new_array[0]
+            new_array.unshift(removed_num)
+            break
+          elsif removed_num > new_array[counter] && removed_num <= new_array[counter + 1]
+            new_array.insert(counter + 1, removed_num)
+            break
+          else
+            #nothing
+          end
+        else #counter + 1 != nil
+          new_array.push(removed_num)
+          break
+        end
+        counter += 1
+      end
+    end
 
-first_array = [1, 2, 9, 0, 8]
-new_array = []
-counter = 0
-counter2 = first_array.length
-
-while first_array.length > 0
-
-#chop off first digit of first_array
-number = first_array.slice!(0)
-puts "----"
-puts new_array
-#compare digit to everything inside new array1
-while counter2 > 0
-  puts "working"
-  if new_array.length == 0
-    new_array.push(number)
-    counter += 1
-    counter2 -+ 1
-  elsif
-    new_array[counter - 1] < number
-    new_array.push(number)
-    counter += 1
-    counter2 -+ 1
-  elsif
-    new_array[counter - 1] > number
-    new_array.pop(number)
-    counter += 1
-    counter2 -+ 1
-  else
-    puts "ERROR"
-end
-end
-end
 puts new_array
