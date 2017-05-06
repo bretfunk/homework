f8671041587dbc6cd05501ca87c416b5a25bdcb2
BFunk <bret.e.funk@gmail.com>
2017-05-06 16:42:56 -0600

    update

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,28 +1,41 @@
+class InsertionSort
 
-array = [1, 3, 5, 6, 2, 2, 2, 2, 9, 0, 0, 0, 0, 4, 5, 6, 6, 6, 1, 1, 1, 1]
-new_array = [4, 4, 4, 4]
-
+  def sort(array)
+    new_array = []
     array.length.times do
-      counter = 0
-      removed_num = array.shift
-
+    counter = 0
+    removed_num = array.shift
       while counter <= new_array.length
-        if new_array[counter + 1] != nil
+        if new_array.length == 1 #removing the new_array[1] error that will always execute counter + 1 == nil no matter what
+          if removed_num > new_array[0]
+            new_array.push(removed_num)
+            break
+          else
+            new_array.unshift(removed_num)
+            break
+          end
+        elsif [counter + 1] == nil #if counter reaches the end of the new_array without being less than something then add it to the end
+          new_array.push(removed_num)
+          break
+        elsif new_array[counter + 1] != nil
           if removed_num <= new_array[0]
             new_array.unshift(removed_num)
             break
-          elsif removed_num > new_array[counter] && removed_num <= new_array[counter + 1]
+          elsif removed_num > new_array[counter] && removed_num <= new_array[counter + 1] #if removed_num is < some numbers and > other numbers, insert it
             new_array.insert(counter + 1, removed_num)
             break
-          else
-            #nothing
           end
-        else #counter + 1 != nil
+        else #this was kept because it was in an earlier version.  If I remove it the program won't run and I don't understand why
           new_array.push(removed_num)
           break
         end
         counter += 1
       end
     end
+    puts new_array.inspect #return result in a beautiful array format
+  end #end sort method
 
-puts new_array
+end #end class
+
+sorter = InsertionSort.new
+sorter.sort(["d", "b", "a", "c"])
